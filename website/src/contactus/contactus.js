cd9af6c61734a3483bc05ef0d5941c943faf87b5
Uthistran Selvaraj <uthistran@gmail.com>
2020-09-17 19:41:58 -0700

    implemented Menubar and routing

---
+++ website/src/contactus/contactus.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class ContactUs extends React.Component{
+    render(){
+        return(
+            <div>ContactUS Page</div>
+        )
+    }
+}
+export default ContactUs;
