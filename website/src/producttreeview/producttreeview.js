fb76aee91aed401a958d8e6532b9579033bcdd92
Uthistran Selvaraj <uthistran@gmail.com>
2020-09-23 17:53:11 -0700

    Completed Product Tree View

---
+++ website/src/producttreeview/producttreeview.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './producttreeview.css'
+import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBadge } from "mdbreact";
+import DataContext from '../helper/datacontext';
+
+class ProductTreeView extends React.Component {
+
+    static contextType = DataContext;
+
+    render() {
+        let product = this.context.product;
+        return (
+            <MDBContainer className='productTreeView'>
+                <MDBListGroup className='productTreeList'>
+                    {
+                        this.getProduct(product.ProductDetails)
+                    }
+                </MDBListGroup>
+            </MDBContainer>
+        )
+    }
+
+
+    getProduct(productDetails) {
+        let element = null;
+        if(productDetails){
+            element = productDetails.map((product, index) => {
+                return (
+                    <MDBListGroupItem key={index} className="d-flex justify-content-between align-items-center productTreeItem">
+                        {product.mainproduct}
+                        <MDBBadge className='productTreeBadge' color="gray-dark" pill>{product.subproducts.length}</MDBBadge>
+                    </MDBListGroupItem>
+                )
+            })
+        }
+        return element
+    }
+}
+
+export default ProductTreeView;
---
+++ website/src/productview/productview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './productview.css'
+
+class ProductView extends React.Component{
+    render(){
+        return(
+            <div className='productView'></div>
+        )
+    }
+}
+
+export default ProductView
