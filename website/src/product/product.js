4ebdb98740219713d81b50bbe300927495c78a11
Uthistran Selvaraj <uthistran@gmail.com>
2020-09-24 19:02:08 -0700

    displayed productview

--- website/src/product/product.js
+++ website/src/product/product.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import DataContext from '../helper/datacontext';
 import ProductTreeView from '../producttreeview/producttreeview';
 import ProductView from '../productview/productview';
 
 class Product extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            productviewlist : []
+        }
+    }
+    static contextType = DataContext;
     render() {
         return (
             <div>
                 <ProductTreeView></ProductTreeView>
-                <ProductView></ProductView>
+                <ProductView products={this.state.productviewlist}></ProductView>
             </div>
         )
+    }
+
+    componentDidMount(){
+        let product = this.context.product;
+        var productList;
+        if(product && product.ProductDetails){
+            productList = product.ProductDetails.map((product, index) => {
+                return product.subproducts
+            })
+        }
+        productList = productList.flat();
+        this.setState({
+            productviewlist : productList
+        })
     }
 }
 export default Product;
--- website/src/productview/productview.js
+++ website/src/productview/productview.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import './productview.css'
+import { MDBCard, MDBCardBody, MDBCardText } from 'mdbreact';
 
-class ProductView extends React.Component{
-    render(){
-        return(
-            <div className='productView'></div>
+class ProductView extends React.Component {
+    render() {
+        return (
+            <div className='productView'>
+                {this.getProductList()}
+            </div>
         )
+    }
+
+    getProductList() {
+        
+        var element =  this.props.products.map((product, index) => {
+            return (
+                <MDBCard  md="3" key={index} className="productViewCard">
+                    <MDBCardBody>
+                        <MDBCardText>
+                            {product.name}
+                        </MDBCardText>
+                        <div>{product.price}</div>
+                        <div>{product.discount}</div>
+                    </MDBCardBody>
+                </MDBCard>
+            )
+        })
+       
+
+        return element;
     }
 }
 
