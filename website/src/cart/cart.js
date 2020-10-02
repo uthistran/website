917df0711b1af6416a1303c01708026669f17f3b
Uthistran Selvaraj <uthistran@gmail.com>
2020-10-02 08:54:12 -0700

    started cart display

--- website/src/cart/cart.js
+++ website/src/cart/cart.js
@@ -2,6 +2,7 @@ import { MDBBadge } from 'mdbreact';
 import React from 'react';
 import './cart.css'
 import { MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+import { numberFormat } from '../helper/formatnumber';
 
 
 class Cart extends React.Component {
@@ -44,16 +45,30 @@ class Cart extends React.Component {
         )
     }
 
-    getCardDetails(cartData){
-        return(
+    getCardDetails(cartData) {
+        return (
             cartData.map((element, index) => {
                 return (
-                    <div>
-                        
+                    <div key={index} className='cartItem'>
+                        <div className='cartItemName'>{element.name}</div>
+                        <div className='cartItemImage'>
+                            <img src={this.getImagePath(element.image)} alt ={element.name}></img> </div>
+                        <label className='cartItemPrice'>{this.getDiscountedPrice(element.price, element.discount)}</label>
                     </div>
                 )
             })
         )
+    }
+
+    getImagePath(image){
+        return '/images/' + image;
+    }
+
+    getDiscountedPrice(price, discount) {
+        var priceinNum = Number(price);
+        var discountinNum = Number(discount.replace('%', ''))
+        var discountedPrice = (priceinNum / 100) * discountinNum
+        return numberFormat(discountedPrice.toString());
     }
 }
 
