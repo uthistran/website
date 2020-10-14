bba3546d5f0954f67867a812555f11271c3d727a
Uthistran Selvaraj <uthistran@gmail.com>
2020-10-14 05:33:11 -0700

    usehistory to navigate from homepage to productpage

--- website/src/home/homeproduct.js
+++ website/src/home/homeproduct.js
@@ -1,25 +1,30 @@
 import { MDBCard, MDBCardBody, MDBCardText } from 'mdbreact';
 import React from 'react'
 import './home.css'
+import { useHistory } from "react-router-dom";
 
-class HomeProduct extends React.Component {
-    render() {
-        let product = this.props.product;
-        return (
-            <MDBCard md="3" className="homeCard">
-                <MDBCardBody>
-                    <MDBCardText className='mainName'>
-                        {product.mainproduct}
-                    </MDBCardText>
-                    <div className='mainImage'>
-                        <img alt={product.mainproduct} src={this.getImageSrc(product.mainimage)} />
-                    </div>
-                </MDBCardBody>
-            </MDBCard>
-        )
+function HomeProduct(props) {
+    const history = useHistory();
+
+    let product = props.product;
+    return (
+        <MDBCard md="3" className="homeCard" onClick={() => handleClick(product.mainproduct)}>
+            <MDBCardBody>
+                <MDBCardText className='mainName'>
+                    {product.mainproduct}
+                </MDBCardText>
+                <div className='mainImage'>
+                    <img alt={product.mainproduct} src={getImageSrc(product.mainimage)} />
+                </div>
+            </MDBCardBody>
+        </MDBCard>
+    )
+
+    function handleClick(value){
+        history.push("/product");
     }
 
-    getImageSrc(path){
+    function getImageSrc(path){
         return '/images/' + path;
     }
 }
