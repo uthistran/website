069d81667156f8450d60c1b5a14446a4cb184472
Uthistran Selvaraj <uthistran@gmail.com>
2020-10-13 18:13:37 -0700

    showing a video in home page

--- website/src/home/home.js
+++ website/src/home/home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-class Home extends React.Component{
-    render(){
-        return(
-            <div>Home Page</div>
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <video id='vid' width="100%" height="100%" autoPlay loop muted playsInline>
+                    <source src='/video/firework.mp4' type="video/mp4"></source>
+                </video>
+            </div>
         )
     }
 }
