c7dedbf98e0840732e77d6e57c29d5654bf26f7c
Uthistran Selvaraj <uthistran@gmail.com>
2020-10-19 15:56:55 -0700

    fixing video by hosting in glitch

--- website/src/home/home.js
+++ website/src/home/home.js
@@ -4,14 +4,14 @@ import HomeProduct from './homeproduct';
 import './home.css'
 
 class Home extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            randomArray : []
+            randomArray: []
         }
         this.handleCardClick = this.handleCardClick.bind(this);
     }
-    handleCardClick(value){
+    handleCardClick(value) {
         this.context.updateActiveProductView(value);
     }
     static contextType = DataContext;
@@ -20,7 +20,7 @@ class Home extends React.Component {
         return (
             <div className='home'>
                 <video id='vid' width="100%" height="100%" autoPlay loop muted playsInline>
-                    <source src={process.env.PUBLIC_URL + '/video/firework.mp4'} type="video/mp4"></source>
+                    <source src={"https://cdn.glitch.com/9c6632f8-26c9-40c9-aad2-278ed150319f%2FFirework%20-%2032826%20(1).mp4?v=1603147567502"} type="video/mp4"></source>
                 </video>
                 <div className='cards'>
                     {this.getMainProducts(products.ProductDetails)}
@@ -29,23 +29,23 @@ class Home extends React.Component {
         )
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let element = [];
-        for(let i=0; i< 6; i++){
+        for (let i = 0; i < 6; i++) {
             let randomNum = this.randomIntFromInterval(0, 10);
             if (element.indexOf(randomNum) < 0)
                 element.push(randomNum)
         }
         this.setState({
-            randomArray : element
+            randomArray: element
         })
     }
 
-    getMainProducts(products){
+    getMainProducts(products) {
         let obj = [];
-        if(products){
+        if (products) {
             this.state.randomArray.forEach((value, index) => {
-                if(products[value]){
+                if (products[value]) {
                     obj.push(<HomeProduct handleCardClick={this.handleCardClick} key={index} product={products[value]}></HomeProduct>);
                 }
             })
